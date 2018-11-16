e35bf2dfd970536192b417e7cf00273e97b23456
Jack <lijaack@users.noreply.github.com>
2018-11-15 23:11:55 -0800

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,51 @@
 $( document ).ready(function(){
 
     $("#start").click(function(){
+        $("#info").empty();
         var correct=0;
         var incorrect=0;
         var unanswered=0;
         var time=30; 
-        setIntervalID=setInterval(timer, 1000);      
+        setIntervalID=setInterval(timer, 1000);
+        var trivia=[
+            {question:"hells123dfo?",
+            answers:[1,2,3,4],
+            answer:3},
+
+            {question:"hell4sdfo?",
+            answers:[1,2,3,4],
+            answer:3} ,
+
+            {question:"hellsdfo?",
+            answers:[1,2,3,4],
+            answer:3} ,
+
+            {question:"helsd543flo?",
+            answers:[1,2,3,4],
+            answer:3},
+
+            {question:"helsd345flo?",
+            answers:[1,2,3,4],
+            answer:3},
+
+            {question:"helsdf345lo?",
+            answers:[1,2,3,4],
+            answer:3} ,
+
+            {question:"helldfso123?",
+            answers:[1,2,3,4],
+            answer:3} ,
+
+            {question:"helasdflo?123",
+            answers:[1,2,3,4],
+            answer:3} 
+        ]      
+        for (i=0; i<trivia.length;i++){
+            console.log(trivia[i].question)
+        }
+
+
+
 
         function timer(){
             if (time===0){
