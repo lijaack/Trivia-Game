12c313bb5c312969fd3cd7a286031fe1b9923d99
Jack <lijaack@users.noreply.github.com>
2018-11-16 19:32:32 -0800

    finished questions and radio buttons
    have to get inputs and compare answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,46 +7,7 @@ $( document ).ready(function(){
         var unanswered=0;
         var time=30; 
         setIntervalID=setInterval(timer, 1000);
-        var trivia=[
-            {question:"hells123dfo?",
-            answers:[1,2,3,4],
-            answer:3},
-
-            {question:"hell4sdfo?",
-            answers:[1,2,3,4],
-            answer:3} ,
-
-            {question:"hellsdfo?",
-            answers:[1,2,3,4],
-            answer:3} ,
-
-            {question:"helsd543flo?",
-            answers:[1,2,3,4],
-            answer:3},
-
-            {question:"helsd345flo?",
-            answers:[1,2,3,4],
-            answer:3},
-
-            {question:"helsdf345lo?",
-            answers:[1,2,3,4],
-            answer:3} ,
-
-            {question:"helldfso123?",
-            answers:[1,2,3,4],
-            answer:3} ,
-
-            {question:"helasdflo?123",
-            answers:[1,2,3,4],
-            answer:3} 
-        ]      
-        for (i=0; i<trivia.length;i++){
-            console.log(trivia[i].question)
-        }
-
-
-
-
+        
         function timer(){
             if (time===0){
                 clearInterval(setIntervalID)
@@ -54,17 +15,80 @@ $( document ).ready(function(){
             $("#info").html("<h2>Time Remaining: " + time + " seconds!</h2>")
             time--
         }
-    
-
         $("#info").append("<h2>Time Remaining: " + time + " seconds!</h2>")
 
+        const trivia=[
+            {question:"what is 1+1?",
+            answers:[1,2,3,4],
+            answer:2},
 
+            {question:"what color is an orange?",
+            answers:["yellow","green","orange","black"],
+            answer:"orange"} ,
 
+            {question:"select the right answer!",
+            answers:["the right answer","the wrong answer","the wrong answer","the wrong answer"],
+            answer:"the right answer"} ,
 
+            {question:"who is sponge bob's best friend?",
+            answers:["squidward","patrick","plankton","mr.krab"],
+            answer:"patrick"},
 
+            {question:"Who is the fastest hero?",
+            answers:["iron man","bat man","the flash", "captain america"],
+            answer:"the flash"},
 
+            {question:"what is 5+5?",
+            answers:[10,4,6,2],
+            answer:10} ,
 
+            {question:"what color is spongebob?",
+            answers:["black","blue","yellow","green"],
+            answer:"yellow"} ,
 
+            {question:"what color is patrick?",
+            answers:["white","orange","blue","pink"],
+            answer:"pink"} 
+        ]      
+        for (i=0; i<trivia.length;i++){
+            var nextTrivia = $("<div class='container justify-content-center'>");
+
+            var question = $("<div class='row justify-content-center'><h3>" + trivia[i].question+ "</h3></div>");
+
+            nextTrivia.append(question);
+
+            var answer1 = $("<label class='buttonSpace'><input name='"+ trivia[i].question + "' type='radio' option='"+ trivia[i].answers[0]+ "'>" + trivia[i].answers[0] + "</label>")
+            var answer2 = $("<label class='buttonSpace'><input name='"+ trivia[i].question + "' type='radio' option='"+ trivia[i].answers[1]+ "'>" + trivia[i].answers[1] + "</label>")
+            var answer3 = $("<label class='buttonSpace'><input name='"+ trivia[i].question + "' type='radio' option='"+ trivia[i].answers[2]+ "'>" + trivia[i].answers[2] + "</label>")
+            var answer4 = $("<label class='buttonSpace'><input name='"+ trivia[i].question + "' type='radio' option='"+ trivia[i].answers[3]+ "'>" + trivia[i].answers[3] + "</label>")
+
+            var options = $("<div class='row justify-content-center'>")
+            options.append(answer1, answer2, answer3, answer4);
+            nextTrivia.append(options);
+
+            var correct = trivia[i].answer;
+            
+            $(".divYellow").append(nextTrivia);
+
+        }
+
+    
+
+        var finishButton = $("<div class='row justify-content-center'><button class='btn-end' id='end'> finish </button></div>");
+
+        $(".divYellow").append (finishButton);
+
+        $("#end").click(checkAnswers());
+                
+        if (time===0){
+            checkAnswers();
+        }    
+       
+
+        function checkAnswers(){
+            //grab input values and compare with correct answers 
+            //empty page
+            //return results
 
 
 
